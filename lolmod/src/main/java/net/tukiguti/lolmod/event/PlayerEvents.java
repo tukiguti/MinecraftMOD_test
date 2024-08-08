80dec755ef013fc5bd13f0aadf8695d26ce5fada
Shinada <134918637+tukiguti@users.noreply.github.com>
2024-08-08 17:39:12 +0900

    一部デバッグログの削除

--- lolmod/src/main/java/net/tukiguti/lolmod/event/PlayerEvents.java
+++ lolmod/src/main/java/net/tukiguti/lolmod/event/PlayerEvents.java
@@ -9,22 +9,14 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.tukiguti.lolmod.LolMod;
 import net.tukiguti.lolmod.stat.mana.ManaSystem;
 import net.tukiguti.lolmod.stat.mana.capability.ManaProvider;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class PlayerEvents {
-    private static final Logger LOGGER = LogManager.getLogger();
 
     @SubscribeEvent
     public static void onAttachCapabilities(AttachCapabilitiesEvent<Entity> event) {
         if (event.getObject() instanceof Player player) {
             if (!event.getObject().getCapability(ManaSystem.MANA).isPresent()) {
                 event.addCapability(new ResourceLocation(LolMod.MOD_ID, "mana"), new ManaProvider());
-                LOGGER.info("Mana capability attached to player: {}",
-                        player.getGameProfile() != null ? player.getGameProfile().getName() : "Unknown");
-            } else {
-                LOGGER.debug("Mana capability already present for player: {}",
-                        player.getGameProfile() != null ? player.getGameProfile().getName() : "Unknown");
             }
         }
     }
@@ -34,7 +26,6 @@ public class PlayerEvents {
         Player player = event.getEntity();
         player.getCapability(ManaSystem.MANA).ifPresent(mana -> {
             mana.updateMaxMana(player);
-            LOGGER.info("Player {} leveled up. New max mana: {}", player.getGameProfile().getName(), mana.getMaxMana());
         });
     }
 }
